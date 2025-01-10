cae4986994a027547deca12ef0dce2cbea55217c
Meesz <20428509+Meesz@users.noreply.github.com>
2025-01-11 00:08:53 +0100

    Refactor loading_spinner module: Add LoadingSpinner class for displaying a loading spinner animation

--- views/loading_spinner.py
+++ views/loading_spinner.py
@@ -1,3 +1,7 @@
+"""
+This module contains the LoadingSpinner class, which is responsible for displaying a loading spinner animation.
+"""
+
 from PyQt6.QtWidgets import QWidget
 from PyQt6.QtCore import Qt, QTimer, QRectF
 from PyQt6.QtGui import QPainter, QColor
