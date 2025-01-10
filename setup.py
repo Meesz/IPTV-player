1c09e4ee1a905b4364ab9dc02af26e94296de736
Meesz <20428509+Meesz@users.noreply.github.com>
2025-01-10 19:24:44 +0100

    Add packages to setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name="Simple IPTV",
     version="0.1.0",
     description="A simple IPTV player with EPG support",
+    packages=['models', 'utils', 'views', 'controllers'],
     executables=[Executable("main.py", base="Win32GUI")],
-) 
+)
