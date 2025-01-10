313e6e553fa46ab5095f4c08b22ba38904901dc6
Meesz <20428509+Meesz@users.noreply.github.com>
2025-01-10 18:48:34 +0100

    Refactor setup.py: Update project name and add executable configuration

--- setup.py
+++ setup.py
@@ -1,32 +1,8 @@
-from setuptools import setup, find_packages
+from cx_Freeze import setup, Executable
 
 setup(
-    name="simple-iptv",
+    name="Simple IPTV",
     version="0.1.0",
-    packages=find_packages(),
-    install_requires=[
-        "PyQt6>=6.0.0",
-        "python-vlc>=3.0.0",
-        "requests>=2.25.0",
-    ],
-    entry_points={
-        'console_scripts': [
-            'simple-iptv=simple_iptv.main:main',
-        ],
-    },
-    author="Meesz",
-    author_email="meesz@meesz.com",
     description="A simple IPTV player with EPG support",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/Meesz/IPTV-player",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Development Status :: 3 - Alpha",
-        "Environment :: X11 Applications :: Qt",
-        "Topic :: Multimedia :: Video :: Display",
-    ],
-    python_requires=">=3.7",
+    executables=[Executable("main.py", base="Win32GUI")],
 ) 
