1ac6e446e740171765a76bf05ac931f7313744d6
Meesz <20428509+Meesz@users.noreply.github.com>
2025-01-10 22:28:54 +0100

    Refactor setup.py: Remove unnecessary code for packaging the application

--- setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="Simple IPTV",
-    version="0.1.0",
-    description="A simple IPTV player with EPG support",
-    packages=['models', 'utils', 'views', 'controllers'],
-    executables=[Executable("main.py", base="Win32GUI")],
-)
