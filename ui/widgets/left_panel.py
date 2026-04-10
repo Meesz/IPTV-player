e2a4510f91a253544aed900058428bac8a7bb01f
Meesz <meesvanberkel120@hotmail.com>
2026-04-10 05:28:58 +0200

    update ui/widgets/left_panel.py

--- ui/widgets/left_panel.py
+++ ui/widgets/left_panel.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QFrame, QVBoxLayout, QComboBox, QTabWidget, QListWidget
+from PyQt6.QtWidgets import QFrame, QVBoxLayout, QComboBox, QTabWidget, QListWidget, QListWidgetItem
+from PyQt6.QtCore import Qt
 from ui.widgets.epg_widget import EPGWidget
 from ui.widgets.search_bar import SearchBar
 
@@ -34,3 +35,25 @@ class LeftPanel(QFrame):
         # Add EPG widget
         self.epg_widget = EPGWidget()
         layout.addWidget(self.epg_widget)
+
+    def clear_channels(self):
+        self.channel_list.clear()
+        self.favorites_list.clear()
+
+    def add_channels(self, channels):
+        self.channel_list.clear()
+        for channel in channels:
+            item = self._channel_item(channel.name, channel)
+            self.channel_list.addItem(item)
+
+    def add_favorites(self, channels):
+        self.favorites_list.clear()
+        for channel in channels:
+            item = self._channel_item(channel.name, channel)
+            self.favorites_list.addItem(item)
+
+    @staticmethod
+    def _channel_item(name, channel):
+        item = QListWidgetItem(name)
+        item.setData(Qt.ItemDataRole.UserRole, channel)
+        return item
