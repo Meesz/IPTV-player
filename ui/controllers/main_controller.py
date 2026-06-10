4f11fbf26ffabd54bc5ddea81e2fe5c6dfc794b2
Meesz <meesvanberkel120@hotmail.com>
2026-06-10 02:21:23 +0200

    update ui/controllers/main_controller.py

--- ui/controllers/main_controller.py
+++ ui/controllers/main_controller.py
@@ -12,10 +12,12 @@ class MainController:
                 return
 
         last_source = self.settings_controller.get_setting("last_playlist_source_type", "")
-        last_playlist = self.settings_controller.get_setting("last_playlist_path", "") or self.settings_controller.get_setting("last_playlist", "")
-        last_playlist_is_url = str(
-            self.settings_controller.get_setting("last_playlist_is_url", "false")
-        ).strip().lower()
+        last_playlist = self.settings_controller.get_setting(
+            "last_playlist_path", ""
+        ) or self.settings_controller.get_setting("last_playlist", "")
+        last_playlist_is_url = (
+            str(self.settings_controller.get_setting("last_playlist_is_url", "false")).strip().lower()
+        )
         is_url = last_source == "url" or last_playlist_is_url in {"1", "true", "yes", "on"}
         if last_playlist_is_url not in {"1", "true", "yes", "on", "0", "false", "no", "off"}:
             is_url = str(last_playlist).startswith(("http://", "https://"))
