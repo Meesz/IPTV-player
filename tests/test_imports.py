684ac30b11aaa0d56ef24ad07922abda56aa57f5
Meesz <meesvanberkel120@hotmail.com>
2026-06-10 02:21:23 +0200

    update tests/test_imports.py

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,5 +1,6 @@
-import sys
 import os
+import sys
+
 sys.path.append(os.getcwd())
 
 
@@ -7,13 +8,13 @@ def test_imports_smoke():
     from core.models import Channel, Playlist, Settings
     from core.services.history_service import HistoryService
     from core.services.playlist_service import PlaylistService
+    from core.services.settings_service import SettingsService
     from infra.db.history_repository import HistoryRepository
     from infra.db.sqlite_connection import SQLiteConnection
     from infra.parsers.m3u_parser import M3UParser
     from ui.controllers.history_controller import HistoryController
     from ui.controllers.main_controller import MainController
     from ui.controllers.playlist_controller import PlaylistController
-    from core.services.settings_service import SettingsService
 
     assert Channel("name", "url")
     assert Playlist()
