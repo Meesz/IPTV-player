cc8fe2327a80ddd968b43887d22a11d4d1d607c2
Meesz <meesvanberkel120@hotmail.com>
2026-06-10 02:21:23 +0200

    update infra/db/favorites_repository.py

--- infra/db/favorites_repository.py
+++ infra/db/favorites_repository.py
@@ -1,10 +1,12 @@
-from typing import List
 import logging
-from infra.db.sqlite_connection import SQLiteConnection
-from core.models import Channel
+from typing import List
+
 from core.errors import RepositoryError
+from core.models import Channel
+from infra.db.sqlite_connection import SQLiteConnection
 
 logger = logging.getLogger(__name__)
+
 
 class FavoritesRepository:
     def __init__(self, db: SQLiteConnection):
@@ -24,7 +26,7 @@ class FavoritesRepository:
                         channel.group,
                         channel.logo,
                         channel.epg_id,
-                    )
+                    ),
                 )
             return True
         except Exception as exc:
@@ -47,14 +49,17 @@ class FavoritesRepository:
         try:
             with self.db.get_connection() as conn:
                 cursor = conn.execute("SELECT * FROM favorites")
-                return [Channel(
-                    name=row["name"],
-                    url=row["url"],
-                    playlist_path=row["playlist_path"] or "",
-                    group=row["group_name"],
-                    logo=row["logo"],
-                    epg_id=row["epg_id"]
-                ) for row in cursor.fetchall()]
+                return [
+                    Channel(
+                        name=row["name"],
+                        url=row["url"],
+                        playlist_path=row["playlist_path"] or "",
+                        group=row["group_name"],
+                        logo=row["logo"],
+                        epg_id=row["epg_id"],
+                    )
+                    for row in cursor.fetchall()
+                ]
         except Exception as exc:
             logger.error("Failed to read favorites: %s", exc)
             raise RepositoryError("Failed to read favorites") from exc
