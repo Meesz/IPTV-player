670902832fe924e2f6b6772a2f53b0baedf8dfb2
Meesz <meesvanberkel120@hotmail.com>
2026-06-22 20:38:57 +0200

    update ui/controllers/playlist_controller.py

--- ui/controllers/playlist_controller.py
+++ ui/controllers/playlist_controller.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from PyQt6.QtCore import QObject, QThreadPool, pyqtSignal
 
-from core.errors import NetworkError, ParsingError, RepositoryError, ValidationError
+from core.errors import RepositoryError, ValidationError
 from core.models import Playlist, PlaylistReference, PlaylistSourceType
 from core.services.playlist_service import PlaylistService
 from ui.controllers.background_tasks import BackgroundTask, create_background_task
